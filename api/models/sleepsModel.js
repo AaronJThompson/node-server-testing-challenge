a50ec58393b7c80096c9c4ff8345300801e152dd
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-25 13:12:50 +0100

    Sleep model skeleton code added

---
+++ api/models/sleepsModel.js
@@ -0,0 +1,21 @@
+const db = require('../../data/dbConfig');
+
+function insert(sleep) {
+    return null;
+}
+
+function update(fields, id) {
+    return null;
+}
+
+function remove(id) {
+    return null;
+}
+
+function find() {
+    return null;
+}
+
+function findById(id) {
+    return null;
+}
