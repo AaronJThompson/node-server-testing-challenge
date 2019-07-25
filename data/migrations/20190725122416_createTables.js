5f3ba489d5cc55c4604bdf5ba645733624754cd8
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-25 13:04:30 +0100

    Add table

--- data/migrations/20190725122416_createTables.js
+++ data/migrations/20190725122416_createTables.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('sleeps', tbl => {
+        tbl.increments();
+        tbl.datetime('start_time');
+        tbl.datetime('stop_time');
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('sleeps');
 };
